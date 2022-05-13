2a42e4e9aecdfc4be535a8b8f136d1e6b796ba5b
shailendra153 <kushwahshailendra12345@gmail.com>
2022-05-14 00:18:21 +0530

    ;s;

--- router/tournament.router.js
+++ router/tournament.router.js
@@ -4,7 +4,7 @@ const tournamentController = require('../controller/tournament.controller');
 const router = express.Router();
 
 router.post('/upload-tournament', tournamentController.uploadTournament);
-router.get('view-tournament/:tournamentId', tournamentController.viewTournament);
+router.get('/view-tournament/:tournamentId', tournamentController.viewTournament);
 router.get('view-tournament-by-organiserId/:organiserId', tournamentController.viewTournamentByOrganiserId);
 router.get('/apply-for-tournament',tournamentController.applyForTournament);
 router.get('/view-tournament',tournamentController.viewTournamentList);
